01092ba7ca474101410bb7d813f9e48e1ecc07ab
p.palej <p.palej@krd.pl>
2017-07-24 12:21:24 +0200

    Masonry

--- EnglishSchool/App_Start/BundleConfig.cs
+++ EnglishSchool/App_Start/BundleConfig.cs
@@ -14,6 +14,12 @@ namespace EnglishSchool
       bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                   "~/Scripts/jquery.validate*"));
 
+      bundles.Add(new ScriptBundle("~/bundles/masonry").Include(
+                  "~/Scripts/masonry.pkgd.min.js"));
+
+      bundles.Add(new ScriptBundle("~/bundles/masonry-init").Include(
+                  "~/Scripts/masonry-init.js"));
+
       // Use the development version of Modernizr to develop with and learn from. Then, when you're
       // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
       bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ EnglishSchool/Scripts/masonry-init.js
@@ -0,0 +1,11 @@
+﻿var $grid = $('.grid').masonry({
+    columnWidth: 10,
+    itemSelector: '.grid-item',
+    gutter: 10,
+    resize: true,
+});
+
+$grid.on('click', '.grid-item-content', function (event) {
+    $(event.currentTarget).parent('.grid-item').toggleClass('is-expanded');
+    $grid.masonry();
+});
