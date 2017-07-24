19b0547eebc4ef86bfe23dd7a8bfd95b23e1eaeb
p.palej <p.palej@krd.pl>
2017-07-24 14:34:31 +0200

    Masonry

--- EnglishSchool/Scripts/masonry-init.js
+++ EnglishSchool/Scripts/masonry-init.js
@@ -1,11 +1,63 @@
-﻿var $grid = $('.grid').masonry({
-    columnWidth: 10,
-    itemSelector: '.grid-item',
-    gutter: 10,
-    resize: true,
+﻿// external js: masonry.pkgd.js
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
 });
 
-$grid.on('click', '.grid-item-content', function (event) {
-    $(event.currentTarget).parent('.grid-item').toggleClass('is-expanded');
-    $grid.masonry();
+$grid.on( 'click', '.grid-item-content', function(event) {
+
+  var itemContent = this;
+  setItemContentPixelSize( itemContent );
+
+  var itemElem = itemContent.parentNode;
+  $(itemElem).toggleClass('is-expanded');
+  // force redraw
+  var redraw = itemContent.offsetWidth;
+  // renable default transition
+  itemContent.style[ transitionProp ] = '';
+
+  addTransitionListener( itemContent );
+  setItemContentTransitionSize( itemContent, itemElem );
+  
+  $grid.masonry();
+
+  $('html, body').animate({
+      scrollTop: $(event.currentTarget).offset().top - 50
+  }, 600);
+  
 });
+
+var docElem = document.documentElement;
+var transitionProp = typeof docElem.style.transition == 'string' ?
+    'transition' : 'WebkitTransition';
+var transitionEndEvent = {
+  WebkitTransition: 'webkitTransitionEnd',
+  transition: 'transitionend'
+}[ transitionProp ];
+
+function setItemContentPixelSize( itemContent ) {
+  var previousContentSize = getSize( itemContent );
+  // disable transition
+  itemContent.style[ transitionProp ] = 'none';
+  // set current size in pixels
+  itemContent.style.width = previousContentSize.width + 'px';
+  itemContent.style.height = previousContentSize.height + 'px';
+}
+
+function addTransitionListener( itemContent ) {
+  // reset 100%/100% sizing after transition end
+  var onTransitionEnd = function() {
+    itemContent.style.width = '';
+    itemContent.style.height = '';
+    itemContent.removeEventListener( transitionEndEvent, onTransitionEnd );
+  };
+  itemContent.addEventListener( transitionEndEvent, onTransitionEnd );
+}
+
+function setItemContentTransitionSize( itemContent, itemElem ) {
+  // set new size
+  var size = getSize( itemElem );
+  itemContent.style.width = size.width + 'px';
+  itemContent.style.height = size.height + 'px';
+}
