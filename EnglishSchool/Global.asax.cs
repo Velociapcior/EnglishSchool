75ee8bd449028ab8d34c797753ca61130d3f6403
Unknown <piotr.palej91@gmail.com>
2017-07-24 21:39:16 +0200

    Castle windsor

--- EnglishSchool/Global.asax.cs
+++ EnglishSchool/Global.asax.cs
@@ -1,21 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Castle.Windsor;
+using Castle.Windsor.Installer;
+using EnglishSchool.Installers;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace EnglishSchool
 {
-  public class MvcApplication : System.Web.HttpApplication
-  {
-    protected void Application_Start()
+    public class MvcApplication : System.Web.HttpApplication
     {
-      AreaRegistration.RegisterAllAreas();
-      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-      RouteConfig.RegisterRoutes(RouteTable.Routes);
-      BundleConfig.RegisterBundles(BundleTable.Bundles);
+        private static IWindsorContainer container;
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            IocContainer.Setup();
+        }
+
+        protected void Application_End()
+        {
+            container.Dispose();
+        }
     }
-  }
 }
---
+++ EnglishSchool/Installers/ControllerInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using EnglishSchool.Controllers;
+using System.Web.Mvc;
+
+namespace EnglishSchool.Installers
+{
+    public class ControllerInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Classes.FromAssemblyContaining<HomeController>()
+                .BasedOn<IController>()
+                .LifestyleTransient());
+        }
+    }
+}
